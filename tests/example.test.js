9bec25750f70e2683aa064d1dbf8c33d6007ad8b
SashaJson <sashamiller666@gmail.com>
2021-01-21 22:56:47 +0200

    init tests

---
+++ tests/example.test.js
@@ -0,0 +1,21 @@
+'use strict';
+
+const puppeteer = require('puppeteer');
+
+describe('My first puppeteer test', () => {
+
+    it('0. should launch the browser', async () => {
+
+        const browser = await puppeteer.launch({
+            headless: false,
+            slowMo: 10,
+            devtools: false
+        });
+
+        const page = await browser.newPage();
+        await page.goto('https://devexpress.github.io/testcafe/example');
+        await browser.close();
+
+    });
+
+});
