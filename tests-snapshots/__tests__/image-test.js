736d73339a87931d2defd8c39fe323e019ee5092
SashaJson <sashamiller666@gmail.com>
2021-01-29 23:13:56 +0200

    added testing element snapshot

--- tests-snapshots/__tests__/image-test.js
+++ tests-snapshots/__tests__/image-test.js
@@ -22,10 +22,21 @@ describe('Snapshot testing', () => {
 
     afterAll(async () => browser.close());
 
-    test('0. homepage snapshot', async () => {
+    test('0. Homepage snapshot', async () => {
+
         await page.goto('https://example.com');
         const image = await page.screenshot();
         expect(image).toMatchSnapshot();
+
+    });
+
+    test('1. Single element snapshot', async () => {
+
+        await page.goto('https://example.com');
+        const h1 = await page.waitForSelector('h1');
+        const image = await h1.screenshot();
+        expect(image).toMatchSnapshot();
+
     });
 
 }); // describe (Snapshot testing)
