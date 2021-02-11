6414e24bb9146fd39650e7fe0bdb801d8b09e16e
SashaJson <sashamiller666@gmail.com>
2021-02-11 21:18:54 +0200

    added to project pptr-funct accessibilityTest

---
+++ puppeteer-functionally/tests/accessibilityTest.test.js
@@ -0,0 +1,15 @@
+const puppeteer = require('puppeteer');
+
+(async () => {
+    //Accessibility Test
+    const browser = await puppeteer.launch({headless: false});
+    const page = await browser.newPage();
+
+    await page.goto('https://pptr.dev');
+    await page.waitForSelector('title');
+
+    const snapshot = await page.accessibility.snapshot();
+    console.log(snapshot);
+
+    await browser.close();
+})();
